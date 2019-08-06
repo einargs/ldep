2b32ef9f0a07e8db6a9cbc59b4d1ffae8a304b15
Einar Strandberg <sd.estrandberg@mtsu.edu>
2019-08-05 21:49:47 -0700

    Initial nix setup

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let pinned-nixpkgs-path = import ./pinned-nixpkgs.nix;
+    pinned-pkgs = import pinned-nixpkgs-path {};
+in { pkgs ? pinned-pkgs }: # if I ever need to override stuff
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    fstar
+  ];
+}
